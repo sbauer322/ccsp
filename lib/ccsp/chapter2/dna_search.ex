74e0b8ff88eb37b66e54775e7e297ad267a243f6
Scott Bauer <sbauer322@gmail.com>
2019-10-03 23:19:28 -0400

    Add type aliases and improve the linear search solution

--- lib/ccsp/chapter2/dna_search.ex
+++ lib/ccsp/chapter2/dna_search.ex
@@ -4,27 +4,44 @@ defmodule CCSP.Chapter2.DnaSearch do
   Corresponds to CCSP in Python, Section 2.1, titled "DNA Search"
   """
 
-  @spec char_to_nucleotide(String.t) :: non_neg_integer
-  def char_to_nucleotide(nucleotide) do
+  @type nucleotide :: non_neg_integer
+
+  # should only ever have exactly 3 elements
+  @type codon :: list(nucleotide)
+
+  @type gene :: list(codon)
+
+  @spec character_to_nucleotide(String.t) :: nucleotide
+  defp character_to_nucleotide(nucleotide) do
+    uppercase = String.upcase(nucleotide)
     cond do
-      nucleotide == "A" -> 0
-      nucleotide == "C" -> 1
-      nucleotide == "G" -> 2
-      nucleotide == "T" -> 3
+      uppercase == "A" -> 0
+      uppercase == "C" -> 1
+      uppercase == "G" -> 2
+      uppercase == "T" -> 3
     end
   end
 
-  @spec string_to_gene(String.t) :: list(list(non_neg_integer))
-  def string_to_gene(str) do
+  @spec string_to_nucleotides(String.t) :: list(nucleotide)
+  def string_to_nucleotides(str) do
     str
     |> String.graphemes()
-    |> Enum.chunk_every(3, 1, :discard)
-
+    |> Enum.reduce([], &([character_to_nucleotide(&1) | &2]))
   end
 
-  @spec linear_contains?(list(list(non_neg_integer)), list(non_neg_integer)) :: boolean
-  def linear_contains?(gene, key_codon) do
-    Enum.any?(gene, &(&1 == key_codon))
+  @spec string_to_gene(String.t) :: gene
+  def string_to_gene(str) do
+    str
+    |> string_to_nucleotides()
+    |> Enum.chunk_every(3, 1, :discard)
+  end
+
+  @spec linear_contains?(gene, codon) :: boolean
+  def linear_contains?(gene, codon) do
+    gene = string_to_gene(gene)
+    codon = string_to_nucleotides(codon)
+
+    Enum.any?(gene, &(&1 == codon))
   end
 
 end
---
+++ test/ccsp/chapter2/dna_search_test.exs
@@ -0,0 +1,26 @@
+defmodule CCSP.Chapter2.DnaSearchTest do
+  require StreamData
+  use ExUnitProperties
+  use ExUnit.Case
+  alias CCSP.Chapter2.DnaSearch
+  @moduledoc false
+
+  test "finds expected matches" do
+    gene = "ACACACCACACGGGACGAT"
+    codon_1 = "ACG"
+    codon_2 = "gat"
+
+    assert DnaSearch.linear_contains?(gene, codon_1)
+    assert DnaSearch.linear_contains?(gene, codon_2)
+  end
+
+  test "finds no matches" do
+    gene = "AAAAAAAAAAAAAAAAAA"
+    codon_1 = "ACG"
+    codon_2 = "gat"
+
+    assert !DnaSearch.linear_contains?(gene, codon_1)
+    assert !DnaSearch.linear_contains?(gene, codon_2)
+  end
+
+end
